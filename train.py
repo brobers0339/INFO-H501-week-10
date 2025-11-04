c0954f401fdbde02f333896ae57bc399226651b6
brobers0339 <brobers@iu.edu>
2025-11-03 21:29:34 -0500

    First try at dtr, did some reworking to better fit the exercise after changes were made

--- train.py
+++ train.py
@@ -1,36 +1,55 @@
+import numpy
 import pandas as pd
 import pickle
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.tree import DecisionTreeRegressor
 
-class Train_Models:
 
-    def __init__(self, url):
-        self.coffee_df = pd.read_csv(url)
-        self.df_train, self.df_test = train_test_split(self.coffee_df, test_size=0.2, random_state=42)
-        self.roast_cat = {}
-        
-    def train_LR(self):
-        features = ['100g_USD']
+def train_lr(df):
+    df_train, df_test = train_test_split(df, test_size=0.2, random_state=42)
 
-        X = self.df_train[features]
-        y = self.df_train['rating']
+    features = ['100g_USD']
 
-        model_1 = LinearRegression()
-        model_1.fit(X, y)
+    X = df_train[features]
+    y = df_train['rating']
 
-        with open('./model_1.pickle', 'wb') as f:
-            pickle.dump(model_1, f)
+    model_1 = LinearRegression()
+    model_1.fit(X, y)
+
+    with open('./model_1.pickle', 'wb') as f:
+        pickle.dump(model_1, f)
             
-    def train_DTR(self):
+    return model_1
+
+def roast_category(roast):
+    roast_cat = {'Light' : 1, 
+                 'Medium-Light' : 2, 
+                 'Medium' : 3, 
+                 'Medium-Dark' : 4, 
+                 'Dark' : 5, 
+                 numpy.nan : 0}
+    return roast.map(roast_cat)
+
+def train_dtr(df):
+    df_train, df_test = train_test_split(df, test_size=0.2, random_state=42)
+    print(df_train.columns)
+    if 'roast' in df_train.columns:
         features = ['100g_USD', 'roast']
+        df_train['roast_cat'] = roast_category(df_train['roast'])
 
-        X = self.df_train[features]
-        y = self.df_train['rating']
+    else:
+        features = df_train.columns
+        df_train['roast_cat'] = df_train['roast_cat'].fillna(0).astype(int)
 
-        model_2 = DecisionTreeRegressor()
-        model_2.fit(X, y)
+    X = df_train[features]
+    y = df_train.values
 
-        with open('./model_2.pickle', 'wb') as f:
-            pickle.dump(model_2, f)
+    model_2 = DecisionTreeRegressor()
+    model_2.fit(X, y)
+
+    with open('./model_2.pickle', 'wb') as f:
+        pickle.dump(model_2, f)
+    
+    return model_2
+
